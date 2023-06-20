35ae78f2815b1767ea4a17c8783fb2f97fb90827
sivaji.lokarapu@factspan.com <sivaji.lokarapu@factspan.com>
2023-06-20 07:36:42 +0000

    initaolise

--- models/customers.sql
+++ models/customers.sql
@@ -1,9 +1,11 @@
 
-{{ config(materialized='table') }}
+{{ config(materialized='table',schema='schema1') }}
 
-SELECT
+with customer as (SELECT
   C_CUSTOMER_SK,
   C_FIRST_NAME,
   C_BIRTH_COUNTRY
 FROM
-  SNOWFLAKE_SAMPLE_DATA.TPCDS_SF100TCL.CUSTOMER;
+  SNOWFLAKE_SAMPLE_DATA.TPCDS_SF100TCL.CUSTOMER)
+
+  select * from customer
