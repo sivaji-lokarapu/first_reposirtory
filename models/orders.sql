55da49ad6ad11ead37b30fffa692a6c87f7f735e
sivaji.lokarapu@factspan.com <sivaji.lokarapu@factspan.com>
2023-06-20 04:38:58 +0000

    initialise

---
+++ models/orders.sql
@@ -0,0 +1,18 @@
+
+{{ config(materialized='table') }}
+
+WITH store_sales AS (
+  SELECT
+    SS_CUSTOMER_SK,
+    SS_SOLD_DATE_SK,
+    SS_ITEM_SK,
+    SS_SALES_PRICE,
+    SS_QUANTITY,
+    SS_STORE_SK
+  FROM
+    SNOWFLAKE_SAMPLE_DATA.TPCDS_SF100TCL.STORE_SALES
+  LIMIT 10000 -- Limiting to 10,000 rows
+)
+
+SELECT *
+FROM store_sales;
